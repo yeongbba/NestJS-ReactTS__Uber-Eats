87f59d57c9d50d2bcf3138b092dfbbdd9efde555
yeongbba <samm162@naver.com>
2020-12-10 20:44:33 +0900

    Create / createOrder resolver & service

--- src/orders/orders.resolver.ts
+++ src/orders/orders.resolver.ts
@@ -1,8 +1,22 @@
+import { User } from './../users/entities/user.entity';
+import { AuthUser } from './../auth/auth-user.decorator';
+import { Role } from './../auth/role.decorator';
+import { CreateOrderOutput, CreateOrderInput } from './dtos/create-order.dto';
 import { OrderService } from './orders.service';
 import { Order } from './entities/order.entity';
-import { Resolver } from '@nestjs/graphql';
+import { Resolver, Mutation, Args } from '@nestjs/graphql';
 
 @Resolver((of) => Order)
 export class OrderResolver {
   constructor(private readonly ordersService: OrderService) {}
+
+  @Mutation((returns) => CreateOrderOutput)
+  @Role(['Client'])
+  async createOrder(
+    @AuthUser() customer: User,
+    @Args('input')
+    createOrderInput: CreateOrderInput,
+  ): Promise<CreateOrderOutput> {
+    return this.ordersService.createOrder(customer, createOrderInput);
+  }
 }
--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,3 +1,8 @@
+import { CreateOrderInput, CreateOrderOutput } from './dtos/create-order.dto';
+import { User } from './../users/entities/user.entity';
+import { Dish } from './../restaurants/entities/dish.entity';
+import { Restaurant } from './../restaurants/entities/restaurant.entity';
+import { OrderItem } from './entities/order-item.entity';
 import { Order } from './entities/order.entity';
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -8,5 +13,83 @@ export class OrderService {
   constructor(
     @InjectRepository(Order)
     private readonly orders: Repository<Order>,
+    @InjectRepository(OrderItem)
+    private readonly orderItems: Repository<OrderItem>,
+    @InjectRepository(Restaurant)
+    private readonly restaurants: Repository<Restaurant>,
+    @InjectRepository(Dish)
+    private readonly dishes: Repository<Dish>,
   ) {}
+
+  async createOrder(
+    customer: User,
+    { restaurantId, items }: CreateOrderInput,
+  ): Promise<CreateOrderOutput> {
+    try {
+      const restaurant = await this.restaurants.findOne(restaurantId);
+      if (!restaurant) {
+        return {
+          ok: false,
+          error: 'Restaurant not found',
+        };
+      }
+      let orderFinalPrice = 0;
+      const orderItems: OrderItem[] = [];
+      for (const item of items) {
+        const dish = await this.dishes.findOne(item.dishId);
+        if (!dish) {
+          return {
+            ok: false,
+            error: 'Dish not found.',
+          };
+        }
+        let dishFinalPrice = dish.price;
+        for (const itemOption of item.options) {
+          const dishOption = dish.options.find(
+            (dishOption) => dishOption.name === itemOption.name,
+          );
+          if (dishOption) {
+            if (dishOption.extra) {
+              dishFinalPrice = dishFinalPrice + dishOption.extra;
+            } else {
+              const dishOptionChoice = dishOption.choices?.find(
+                (optionChoice) => optionChoice.name === itemOption.choice,
+              );
+              if (dishOptionChoice) {
+                if (dishOptionChoice.extra) {
+                  dishFinalPrice = dishFinalPrice + dishOptionChoice.extra;
+                }
+              }
+            }
+          }
+        }
+        orderFinalPrice = orderFinalPrice + dishFinalPrice;
+        const orderItem = await this.orderItems.save(
+          this.orderItems.create({
+            dish,
+            options: item.options,
+          }),
+        );
+        orderItems.push(orderItem);
+      }
+      const order = await this.orders.save(
+        this.orders.create({
+          customer,
+          restaurant,
+          total: orderFinalPrice,
+          items: orderItems,
+        }),
+      );
+      return {
+        ok: true,
+        orderId: order.id,
+      };
+    } catch (e) {
+      console.log(e);
+      return {
+        ok: false,
+        error: 'Could not create order.',
+      };
+    }
+  }
 }
