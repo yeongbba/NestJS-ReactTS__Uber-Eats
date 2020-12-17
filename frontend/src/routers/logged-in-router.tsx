f7628628643def6c588cfec46f965e2a2841525a
yeongbba <samm162@naver.com>
2020-12-17 17:35:30 +0900

    CREATE / make login form & get login data

--- frontend/src/routers/logged-in-router.tsx
+++ frontend/src/routers/logged-in-router.tsx
@@ -1,3 +1,9 @@
-import React from 'react';
+import React from "react";
+import { isLoggedInVar } from "../apollo";
 
-export const LoggedInRouter = () => <span>'Logged in'</span>;
+export const LoggedInRouter = () => (
+  <div>
+    <h1>Logged In</h1>
+    <button onClick={() => isLoggedInVar(false)}>Log Out</button>
+  </div>
+);
